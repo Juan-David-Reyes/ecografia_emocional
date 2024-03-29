628c84f4eb73f478a88d6a82d44c88b285d8c2fd
Bombillo2015 <juan.reyes@davivienda.com>
2024-03-29 11:21:43 -0500

    avance nueva visual

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,27 @@
-$(document).ready(function(){
+function calculateScore() {
+    let score = 0;
 
-    let btn = document.querySelector('.mobile_menu');
-    let iconMenu = document.querySelector('.mobile_menu .fas');
-    let containerMenu = document.querySelector('.main_navbar');
-    let overlay = document.querySelector('.overlay');
-
-    $(btn).click(function(){
-        
-        if( $(btn).hasClass('active') ){
-
-            $(btn).removeClass('active');
-            
-            let myTimeout = setTimeout(myGreeting, 300);
-            $(overlay).css('opacity', '0');
-            
-            function myGreeting() {
-                $(overlay).css('display', 'none');
-            }
-            
-            $(iconMenu).removeClass('fa-times');
-            $(containerMenu).removeClass('active');
-
-        } else {
-            let myTimeout = setTimeout(myGreeting, 100);
-
-
-            $(btn).addClass('active')
-
-            $(overlay).css('display', 'flex');
-            $(overlay).css('transition', '300ms ease all');
-            
-            function myGreeting() {
-                $(overlay).css('opacity', '1');
-            }
-
-            $(iconMenu).addClass('fa-times');
-            $(containerMenu).addClass('active');
-
-        }
-
+    // Obtener todas las respuestas seleccionadas
+    const questions = document.querySelectorAll('#questions div');
+    questions.forEach(question => {
+    const selectedAnswer = question.querySelector('input:checked');
+    if (selectedAnswer) {
+        score += parseInt(selectedAnswer.value);
+    }
     });
 
-
-
-    // Función para agregar la clase al hacer scroll
-function ScrollHeader() {
-    var scrollY = window.scrollY;
-    var elemento = document.querySelector('#header');
-
-    if (scrollY > 120) { // Cambia este valor según tu preferencia
-      elemento.classList.add('scroll');
-    } else {
-      elemento.classList.remove('scroll');
+    // Determinar respuesta según el puntaje
+    let response = '';
+    if (score >= 1 && score <= 9) {
+    response = 'Respuesta para puntajes de 1 a 9';
+    } else if (score >= 10 && score <= 19) {
+    response = 'Respuesta para puntajes de 10 a 19';
+    } else if (score >= 20 && score <= 27) {
+    response = 'Respuesta para puntajes de 20 a 27';
     }
-  }
 
-  // Evento para llamar a la función cuando se hace scroll
-  window.addEventListener('scroll', ScrollHeader);
-
-
-
-
-
-});
-
+    // Mostrar resultado
+   // alert(response);
+}
+        
+calculateScore();
