452602717df1017890869f56ec1c3eefe5e04f5f
Bombillo2015 <juan.reyes@davivienda.com>
2024-03-30 20:44:09 -0500

    avance en logica, estilos y modale

--- App/content.php
+++ App/content.php
@@ -10,7 +10,15 @@
   </header>
   <main>
 
-    <span class="overlay"></span>
+
+    <div class="modalAnswer">
+      <span class="overlay"></span>
+      <div class="infoModal">
+        <h3>Respuesta prueba baja</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia inventore nihil voluptate! Consequuntur voluptatem ad illo quidem nisi. Eligendi commodi vitae debitis voluptate quam minima minus. Ratione deleniti aliquid culpa.</p>
+        <a href="#" class="ctaRedirect">Volver</a>
+      </div>
+    </div>
 
     <div class="mainTitle">
       <h1>Ecografía emocional</h1>
@@ -19,85 +27,90 @@
     
     <form class="container">
 
-      <div class="titCategoria">
-        <h3>Emociones</h3>
-      </div>
       
       
       <div id="questions">
 
-        <div class="cont_quest">
-          <h4>Vocabulario emocional</h4>
-          <div>
-          ¿Cuando te preguntan cómo estás, cuál de las siguientes opciones representa de mejor manera tu vocabulario emocional?.
-          </div>
-        </div>
+      <!-- ************ -->
+      
+      <div class="titCategoria">
+        <h3>Emociones</h3>
+      </div>
+      
+      <!-- ************ -->
 
-        <div class="cont_questions" data-category="1">
-          <label for="c1-q1">
-            <input id="c1-q1" type="radio" name="question1" value="0" required> 
-            <span class="active"></span>
-            <p>Bien, mal, ahí vamos.</p>
-          </label>
-          <label for="c1-q2">
-            <input id="c1-q2" type="radio" name="question1" value="1">
-            <span class="active"></span>
-            <p>Triste, alegre, molesto, miedoso.</p>
-          </label>
-          <label for="c1-q3">
-            <input id="c1-q3" type="radio" name="question1" value="2">
-            <span class="active"></span>
-            <p>Nostálgico, esperanzado, entusiasmado, indignado.</p>
-          </label>
+      <div class="cont_quest">
+        <h4>Vocabulario emocional</h4>
+        <div>
+        ¿Cuando te preguntan cómo estás, cuál de las siguientes opciones representa de mejor manera tu vocabulario emocional?.
         </div>
+      </div>
 
-       <div class="cont_quest">
-          <h4>Validación de la emoción</h4>
-          <div>
-          Cuando me doy cuenta que estoy sintiendo emociones difíciles de manejar como: tristeza, miedo, frustración, enojo, mi actitud suele ser:
-          </div>
-        </div>
-        <div class="cont_questions" data-category="1">
-          <label for="c2-q1">
-            <input id="c2-q1" type="radio" name="question2" value="2" required>
-            <span class="active"></span>
-            <p>Acepto sin juicio la emoción.</p>
-          </label>
-          <label for="c2-q2">
-            <input id="c2-q2" type="radio" name="question2" value="0">
-            <span class="active"></span>
-            <p>Evito sentir la emoción. Considero que no debería estar sintiendo eso.</p>
-          </label>
-          <label for="c2-q3">
-            <input id="c2-q3" type="radio" name="question2" value="1">
-            <span class="active"></span>
-            <p>Al principio me resisto un poco a la emoción, pero termino por aceptarla.</p>
-          </label>
-        </div>
+      <div class="cont_questions cat1" data-category="1">
+        <label for="c1-q1">
+          <input id="c1-q1" type="radio" name="question1" value="0" required> 
+          <span class="active"></span>
+          <p>Bien, mal, ahí vamos.</p>
+        </label>
+        <label for="c1-q2">
+          <input id="c1-q2" type="radio" name="question1" value="1">
+          <span class="active"></span>
+          <p>Triste, alegre, molesto, miedoso.</p>
+        </label>
+        <label for="c1-q3">
+          <input id="c1-q3" type="radio" name="question1" value="2">
+          <span class="active"></span>
+          <p>Nostálgico, esperanzado, entusiasmado, indignado.</p>
+        </label>
+      </div>
 
-        <div class="cont_quest">
-          <h4>Comunicar la emoción</h4>
-          <div>
-          Al momento de compartir lo que siento, cuál de las siguientes afirmaciones te representa mejor:
-          </div>
+      <div class="cont_quest">
+        <h4>Validación de la emoción</h4>
+        <div>
+        Cuando me doy cuenta que estoy sintiendo emociones difíciles de manejar como: tristeza, miedo, frustración, enojo, mi actitud suele ser:
         </div>
-        <div class="cont_questions" data-category="1">
-          <label for="c3-q1">
-            <input id="c3-q1" type="radio" name="question3" value="0" required>
-            <span class="active"></span>
-            <p>Nunca comunico mis emociones</p>
-          </label>
-          <label for="c3-q2">
-            <input id="c3-q2" type="radio" name="question3" value="1">
-            <span class="active"></span>
-            <p>Me cuesta comunicar las emociones pero con personas de confianza logro hacerlo</p>
-          </label>
-          <label for="c3-q3">
-            <input id="c3-q3" type="radio" name="question3" value="2">
-            <span class="active"></span>
-            <p>Me es muy fácil comunicar lo que estoy viviendo</p>
-          </label>
+      </div>
+      <div class="cont_questions cat1" data-category="1">
+        <label for="c2-q1">
+          <input id="c2-q1" type="radio" name="question2" value="2" required>
+          <span class="active"></span>
+          <p>Acepto sin juicio la emoción.</p>
+        </label>
+        <label for="c2-q2">
+          <input id="c2-q2" type="radio" name="question2" value="0">
+          <span class="active"></span>
+          <p>Evito sentir la emoción. Considero que no debería estar sintiendo eso.</p>
+        </label>
+        <label for="c2-q3">
+          <input id="c2-q3" type="radio" name="question2" value="1">
+          <span class="active"></span>
+          <p>Al principio me resisto un poco a la emoción, pero termino por aceptarla.</p>
+        </label>
+      </div>
+
+      <div class="cont_quest">
+        <h4>Comunicar la emoción</h4>
+        <div>
+        Al momento de compartir lo que siento, cuál de las siguientes afirmaciones te representa mejor:
         </div>
+      </div>
+      <div class="cont_questions cat1" data-category="1">
+        <label for="c3-q1">
+          <input id="c3-q1" type="radio" name="question3" value="0" required>
+          <span class="active"></span>
+          <p>Nunca comunico mis emociones</p>
+        </label>
+        <label for="c3-q2">
+          <input id="c3-q2" type="radio" name="question3" value="1">
+          <span class="active"></span>
+          <p>Me cuesta comunicar las emociones pero con personas de confianza logro hacerlo</p>
+        </label>
+        <label for="c3-q3">
+          <input id="c3-q3" type="radio" name="question3" value="2">
+          <span class="active"></span>
+          <p>Me es muy fácil comunicar lo que estoy viviendo</p>
+        </label>
+      </div>
 
 
         <div class="cont_quest">
@@ -106,7 +119,7 @@
           Ante emociones dificiles de manejar (rabia, tristeza, frustración, entre otras), con cuál de las siguientes frases te identificas más:
           </div>
         </div>
-        <div class="cont_questions" data-category="1">
+        <div class="cont_questions cat1" data-category="1">
           <label for="c4-q1">
             <input id="c4-q1" type="radio" name="question4" value="0" required>
             <span class="active"></span>
@@ -124,158 +137,174 @@
           </label>
         </div>
 
+        <!-- ************ -->
+      
+        <div class="titCategoria">
+          <h3>Hábitos - Técnicas básicas</h3>
+        </div>
         
-        <!-- <div class="cont_quest">
-          <h5>Pregunta 4:</h5>
-          <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
-          </div>
-        </div>
-        <div class="cont_questions" data-category="2">
-          <label for="c4-q1">
-            <input id="c4-q1" type="radio" name="question4" value="1">
-            <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </label>
-          <label for="c4-q2">
-            <input id="c4-q2" type="radio" name="question4" value="2">
-            <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </label>
-          <label for="c4-q3">
-            <input id="c4-q3" type="radio" name="question4" value="3">
-            <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </label>
-        </div>
+        <!-- ************ -->
 
-
+        
         <div class="cont_quest">
-          <h5>Pregunta 5:</h5>
+          <h4>Respiración y la relajación</h4>
           <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
+          En cuanto a la práctica de ejercicios de respiración y/o relajación, como técnicas que aportan al bienestar físico y mental, con cual de las siguientes afirmaciones te identificas:
           </div>
         </div>
-        <div class="cont_questions" data-category="2">
+        <div class="cont_questions cat2" data-category="2">
           <label for="c5-q1">
-            <input id="c5-q1" type="radio" name="question5" value="1">
+            <input id="c5-q1" type="radio" name="question5" value="0">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>No conozco esas técnicas</p>
           </label>
           <label for="c5-q2">
-            <input id="c5-q2" type="radio" name="question5" value="2">
+            <input id="c5-q2" type="radio" name="question5" value="1">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Conozco y practico ocasionalmente</p>
           </label>
           <label for="c5-q3">
-            <input id="c5-q3" type="radio" name="question5" value="3">
+            <input id="c5-q3" type="radio" name="question5" value="2">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Las conozco y tengo incorporadas en mi vida. Las practico habitualmente</p>
           </label>
         </div>
 
 
         <div class="cont_quest">
-          <h5>Pregunta 6:</h5>
+          <h4>Visualización y meditación</h4>
           <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
+          En cuanto a la práctica de visualizaciones y/o meditación, como técnicas que aportan al bienestar físico y mental, con cual de las siguientes afirmaciones te identificas:
           </div>
         </div>
-        <div class="cont_questions" data-category="2">
+        <div class="cont_questions cat2" data-category="2">
           <label for="c6-q1">
-            <input id="c6-q1" type="radio" name="question6" value="1">
+            <input id="c6-q1" type="radio" name="question6" value="0">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>No conozco esas técnicas</p>
           </label>
           <label for="c6-q2">
-            <input id="c6-q2" type="radio" name="question6" value="2">
+            <input id="c6-q2" type="radio" name="question6" value="1">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Conozco y practico ocasionalmente</p>
           </label>
           <label for="c6-q3">
-            <input id="c6-q3" type="radio" name="question6" value="3">
+            <input id="c6-q3" type="radio" name="question6" value="2">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Las conozco y tengo incorporadas en mi vida. Las practico habitualmente</p>
           </label>
         </div>
 
 
         <div class="cont_quest">
-          <h5>Pregunta 7:</h5>
+          <h4>Movimiento corporal y autoconsciencia corporal</h4>
           <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
+          En cuanto a la práctica de sesiones de movimiento corporal y autoconsciencia corporal como el yoga y la danza consciente:
           </div>
         </div>
-        <div class="cont_questions" data-category="3">
+        <div class="cont_questions cat2" data-category="2">
           <label for="c7-q1">
-            <input id="c7-q1" type="radio" name="question7" value="1">
+            <input id="c7-q1" type="radio" name="question7" value="0">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>No conozco esas técnicas</p>
           </label>
           <label for="c7-q2">
-            <input id="c7-q2" type="radio" name="question7" value="2">
+            <input id="c7-q2" type="radio" name="question7" value="1">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Conozco y practico ocasionalmente</p>
           </label>
           <label for="c7-q3">
-            <input id="c7-q3" type="radio" name="question7" value="3">
+            <input id="c7-q3" type="radio" name="question7" value="2">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Las conozco y tengo incorporadas en mi vida. Las practico habitualmente</p>
           </label>
         </div>
 
+        <!-- ************ -->
+      
+        <div class="titCategoria">
+          <h3>Procesos Cognitivos</h3>
+        </div>
+        
+        <!-- ************ -->
+
+
         <div class="cont_quest">
-          <h5>Pregunta 8:</h5>
+          <h4>Planeación y Flexibilidad cognitiva</h4>
           <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
+          ¿Cómo reaccionas habitualmente a los cambios?
           </div>
         </div>
-        <div class="cont_questions" data-category="3">
+        <div class="cont_questions cat3" data-category="3">
           <label for="c8-q1">
-            <input id="c8-q1" type="radio" name="question8" value="1">
+            <input id="c8-q1" type="radio" name="question8" value="0">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Me molesta mucho que las cosas se salgan de mis planes</p>
           </label>
           <label for="c8-q2">
-            <input id="c8-q2" type="radio" name="question8" value="2">
+            <input id="c8-q2" type="radio" name="question8" value="1">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Me cuesta al principio aceptarlos, pero luego de un tiempo logro soltarlo</p>
           </label>
           <label for="c8-q3">
-            <input id="c8-q3" type="radio" name="question8" value="3">
+            <input id="c8-q3" type="radio" name="question8" value="2">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Entiendo los cambios como un proceso que hace parte de la vida</p>
           </label>
         </div>
 
         <div class="cont_quest">
-          <h5>Pregunta 9:</h5>
+          <h4>Aceptación</h4>
           <div>
-            ¿Aquí empiezan todas las preguntas referentes a la categoría 1?
+          En cuanto a la noticia del embarazo y las circunstancias que lo rodean: momento de vida en el que llegó, la pareja, estar casados o no, etc.
           </div>
         </div>
-        <div class="cont_questions" data-category="3">
+        <div class="cont_questions cat3" data-category="3">
           <label for="c9-q1">
-            <input id="c9-q1" type="radio" name="question9" value="1">
+            <input id="c9-q1" type="radio" name="question9" value="2">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>El momento y circunstancias en que llegó la noticia del embarazo no generan dificultad para mi</p>
           </label>
           <label for="c9-q2">
-            <input id="c9-q2" type="radio" name="question9" value="2">
+            <input id="c9-q2" type="radio" name="question9" value="1">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Me sigue costando que no se haya dado de la manera/expectativas que tenía</p>
           </label>
           <label for="c9-q3">
-            <input id="c9-q3" type="radio" name="question9" value="3">
+            <input id="c9-q3" type="radio" name="question9" value="0">
             <span class="active"></span>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Me rehuso, no logro integrarlo</p>
           </label>
-        </div> -->
+        </div>
+
+        <div class="cont_quest">
+          <h4>Atención</h4>
+          <div>
+            Frente a estar en embarazo:
+          </div>
+        </div>
+        <div class="cont_questions cat3" data-category="3">
+          <label for="c10-q1">
+            <input id="c10-q1" type="radio" name="question10" value="2">
+            <span class="active"></span>
+            <p>Lo tienes presente todo el día, física y emocionalmente.</p>
+          </label>
+          <label for="c10-q2">
+            <input id="c10-q2" type="radio" name="question10" value="1">
+            <span class="active"></span>
+            <p>Lo recuerdo más claramente cuando alguien o algo lo hace evidente.</p>
+          </label>
+          <label for="c10-q3">
+            <input id="c10-q3" type="radio" name="question10" value="0">
+            <span class="active"></span>
+            <p>Pasa desapercibido cuando estoy enfocado/a en mis actividades regulares.</p>
+          </label>
+        </div>
 
 
       </div>
     
-      <button class="cta" onclick="calculateScore()">Ver Resultado</button>
+      <button id="cta">Ver Resultado</button>
   
     </form>
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,168 @@
-function calculateScore() {
-    // Obtener todas las respuestas seleccionadas
-    const questions = document.querySelectorAll('#questions .cont_questions');
-    let allQuestionsAnswered = true; // Variable para verificar si todas las preguntas tienen respuesta
+document.querySelector('#cta').addEventListener('click', function(event){
 
-    questions.forEach(question => {
-        const selectedAnswer = question.querySelector('input:checked');
-        if (!selectedAnswer) {
-            allQuestionsAnswered = false;
-            return; // Salir del bucle forEach si una pregunta no tiene respuesta
+    event.preventDefault();
+
+    function cat1(){
+
+        // Obtener todas las respuestas seleccionadas
+        const questions = document.querySelectorAll('#questions .cont_questions.cat1');
+        let allQuestionsAnswered = true; // Variable para verificar si todas las preguntas tienen respuesta
+    
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            if (!selectedAnswer) {
+                allQuestionsAnswered = false;
+                return; // Salir del bucle forEach si una pregunta no tiene respuesta
+            }
+        });
+    
+        // Si hay preguntas sin responder, mostrar un mensaje de error y salir de la función
+        if (!allQuestionsAnswered) {
+            alert('Por favor, responde todas las preguntas para generar un resultado.');
+            return;
         }
-    });
-
-    // Si hay preguntas sin responder, mostrar un mensaje de error y salir de la función
-    if (!allQuestionsAnswered) {
-        alert('Por favor, responde todas las preguntas para generar un resultado.');
-        return;
+    
+        // Si todas las preguntas tienen respuesta, calcular el puntaje
+        let score = 0;
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            score += parseInt(selectedAnswer.value);
+        });
+    
+        // Determinar respuesta según el puntaje
+        let response = '';
+        let modal = document.querySelector('.modalAnswer');
+    
+        if (score >= 0 && score <= 2) {
+            //response = 'Bajo';
+            $(modal).fadeIn(300);
+            console.log("Bajo");
+        } else if (score >= 3 && score <= 4) {
+            $(modal).fadeIn(300);
+            console.log("Medio");
+        } else if (score >= 5 && score <= 8) {
+            $(modal).fadeIn(300);
+            console.log("Alto");
+        }
     }
 
-    // Si todas las preguntas tienen respuesta, calcular el puntaje
-    let score = 0;
-    questions.forEach(question => {
-        const selectedAnswer = question.querySelector('input:checked');
-        score += parseInt(selectedAnswer.value);
-    });
+    function cat2(){
 
-    // Determinar respuesta según el puntaje
-    let response = '';
-    if (score >= 0 && score <= 2) {
-        $('.overlay').fadeIn();
-        response = 'Bajo';
-        
-    } else if (score >= 3 && score <= 4) {
-        $('.overlay').fadeIn();
-        response = 'Medio';
-    } else if (score >= 5 && score <= 6) {
-        $('.overlay').fadeIn();
-        response = 'Alto';
+        // Obtener todas las respuestas seleccionadas
+        const questions = document.querySelectorAll('#questions .cont_questions.cat2');
+        let allQuestionsAnswered = true; // Variable para verificar si todas las preguntas tienen respuesta
+    
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            if (!selectedAnswer) {
+                allQuestionsAnswered = false;
+                return; // Salir del bucle forEach si una pregunta no tiene respuesta
+            }
+        });
+    
+        // Si hay preguntas sin responder, mostrar un mensaje de error y salir de la función
+        if (!allQuestionsAnswered) {
+            alert('Por favor, responde todas las preguntas para generar un resultado.');
+            return;
+        }
+    
+        // Si todas las preguntas tienen respuesta, calcular el puntaje
+        let score = 0;
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            score += parseInt(selectedAnswer.value);
+        });
+    
+        // Determinar respuesta según el puntaje
+        let response = '';
+        let modal = document.querySelector('.modalAnswer');
+    
+        if (score >= 0 && score <= 2) {
+            //response = 'Bajo';
+            $(modal).fadeIn(300);
+            console.log("Bajo");
+        } else if (score >= 3 && score <= 4) {
+            $(modal).fadeIn(300);
+            console.log("Medio");
+        } else if (score >= 5 && score <= 8) {
+            $(modal).fadeIn(300);
+            console.log("Alto");
+        }
     }
 
-    // Mostrar resultado
-    alert(response);
-}
+    function cat3(){
 
+        // Obtener todas las respuestas seleccionadas
+        const questions = document.querySelectorAll('#questions .cont_questions.cat3');
+        let allQuestionsAnswered = true; // Variable para verificar si todas las preguntas tienen respuesta
+    
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            if (!selectedAnswer) {
+                allQuestionsAnswered = false;
+                return; // Salir del bucle forEach si una pregunta no tiene respuesta
+            }
+        });
+    
+        // Si hay preguntas sin responder, mostrar un mensaje de error y salir de la función
+        if (!allQuestionsAnswered) {
+            alert('Por favor, responde todas las preguntas para generar un resultado.');
+            return;
+        }
+    
+        // Si todas las preguntas tienen respuesta, calcular el puntaje
+        let score = 0;
+        questions.forEach(question => {
+            const selectedAnswer = question.querySelector('input:checked');
+            score += parseInt(selectedAnswer.value);
+        });
+    
+        // Determinar respuesta según el puntaje
+        let response = '';
+        let modal = document.querySelector('.modalAnswer');
+    
+        if (score >= 0 && score <= 2) {
+            //response = 'Bajo';
+            $(modal).fadeIn(300);
+            console.log("Bajo");
+        } else if (score >= 3 && score <= 4) {
+            $(modal).fadeIn(300);
+            console.log("Medio");
+        } else if (score >= 5 && score <= 8) {
+            $(modal).fadeIn(300);
+            console.log("Alto");
+        }
+    }
+
+    cat1();
+    cat2();
+    cat3();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.ctaRedirect').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let modal = document.querySelector('.modalAnswer');
+    const questions = document.querySelectorAll('#questions .cont_questions input');
+
+    if($(modal).is(':visible')){
+
+        $(questions).prop('checked', false);
+        $(modal).fadeOut(300);
+    }
+});
